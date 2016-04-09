17253196a082bd5f2f49d766327c331247330967
Piotr Galar <fjalar.galar@gmail.com>
2016-04-10 01:42:59 +0200

    Searching for tweets

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ var config = require('./config.js')
 // Initializing a Twitter API client
 var twitter = new Twitter(config.twitter)
 
-// Creating a tweet
-var tweet = {
-  status: "Pio-pio!"
+function post(status) {
+  // Creating a tweet
+  var tweet = {
+    status: status
+  }
+  // Posting the tweet
+  twitter.postTweet(tweet, console.log, console.log)
 }
-// Posting the tweet
-twitter.postTweet(tweet, console.log, console.log)
+
+// Creating a tweet search query
+var query = {
+  'q': 'nyuadhack -filter:retweets -filter:replies',
+  'result\_type': 'recent',
+  'count': 100
+}
+// Searching for tweets matching the query
+twitter.getSearch(query, console.log, console.log)
